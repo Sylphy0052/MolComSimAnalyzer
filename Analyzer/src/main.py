7c6f5774037af0263263b248193a1dea1e6690fa
Sylphy0052 <ma17099@shibaura-it.ac.jp>
2018-05-19 10:31:11 +0900

    update: using Data class from data.py

--- Analyzer/src/main.py
+++ Analyzer/src/main.py
@@ -1,5 +1,31 @@
+import os
+import pickle
+from natsort import natsorted
+import glob
+from data import AllData
+
+PICKLE_FILE = '../pickle.txt'
+DAT_PATH = '../dat/*.dat'
+
+def createDataDict(allFileList):
+    dataDict = {}
+    if os.path.isfile(PICKLE_FILE):
+        with open(PICKLE_FILE, 'rb') as f:
+            dataDict = pickle.load(f)
+    else:
+        count = 1
+        for fileName in allFileList:
+            print("{} / {} - {} file reading...".format(count, len(allFileList), fileName.split("/")[2]))
+            dataDict[fileName] = AllData(fileName)
+            count += 1
+        with open(PICKLE_FILE, 'wb') as f:
+            pickle.dump(dataDict, f)
+
+    return dataDict
+
 def main():
-    pass
+    allFileList = natsorted(glob.glob(DAT_PATH))
+    dataDict = createDataDict(allFileList)
 
 if __name__ == "__main__":
     main()
