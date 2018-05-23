ab8ec331ae1646a253c5b93a05784514ecb86c39
Sylphy0052 <ma17099@shibaura-it.ac.jp>
2018-05-23 13:06:08 +0900

    add: function of draw graph of mean

--- Analyzer/src/analyzer.py
+++ Analyzer/src/analyzer.py
@@ -1,5 +1,7 @@
 from enum import Enum
-import sys
+import sys, os
+import matplotlib.pyplot as plt
+from matplotlib import ticker
 
 COLOR_LIST = ['r', 'b', 'g', 'm', 'y', 'k', 'c', 'r', 'b', 'g', 'm', 'y', 'k', 'c']
 STYLE_LIST = ['-', '--', '-.', ':', '-', '--', '-.', ':', '-', '--', '-.', ':', '-']
@@ -19,16 +21,17 @@ class Mode(Enum):
 
 class Analyzer:
     def __init__(self, dataDict, allFileList):
+        print("Analyze Datas")
         self.dataDict = dataDict
         self.allFileList = allFileList
 
     def getValue(self, data, value):
         if value is XValue.DISTANCE:
             return data.getDistance()
-        elif value is YValue.MEAN:
-            return data.getMean()
         elif value is XValue.DUPLICATION:
             return data.getTxDuplication()
+        elif value is YValue.MEAN:
+            return data.getMean()
         else:
             print("Not Define")
             sys.exit(1)
@@ -36,10 +39,10 @@ class Analyzer:
     def getLabel(self, data, value):
         if value is XValue.DISTANCE:
             return "d={}".format(data.getDistance())
-        elif value is XValue.Duplication:
-            return "".format("SW-ARQ{}-{}".format(data.getTxDuplication(), data.getTxDuplication()))
+        elif value is XValue.DUPLICATION:
+            return "SW-ARQ{}-{}".format(data.getTxDuplication(), data.getTxDuplication())
         else:
-            print("Not Define")
+            print("Not Define {} in getLabel".format(value.name))
             sys.exit(1)
 
     def classifyDataFile(self, fileList, labelValue):
@@ -77,7 +80,7 @@ class Analyzer:
             print(fileName)
             data = self.dataDict[fileName]
             x = self.getValue(data, xValue)
-            label = self.getValue(data, labelValue)
+            label = self.getLabel(data, labelValue)
             if not x in X:
                 X.append(x)
             if not label in labels:
@@ -86,22 +89,92 @@ class Analyzer:
         Y = [[] for i in range(len(labels))]
         for fileName in fileList:
             data = self.dataDict[fileName]
-            x = self.getValue(data, xValue)
             y = self.getValue(data, yValue)
-            label = self.getValue(data, labelValue)
+            label = self.getLabel(data, labelValue)
             for i in range(len(labels)):
                 if label == labels[i]:
                     Y[i].append(y)
                     continue
+
         return [X, Y, labels]
 
+    def defineDirectoryPath(self, xValue, yValue, classifyValue):
+        dirPath = "../compare{}_by_{}".format(yValue.name, xValue.name)
+        if classifyValue is not XValue.ALL:
+            dirPath += "_each_{}".format(classifyValue.name)
+        dirPath = dirPath.lower()
+
+        if not os.path.isdir(dirPath):
+            os.makedirs(dirPath)
+
+        return dirPath
+
+    def defineFigName(self, classifyValue, data):
+        figName = ""
+        if classifyValue is XValue.ALL:
+            figName = "all.png"
+        else:
+            if classifyValue is XValue.DISTANCE:
+                figName = "TxRx{}.png".format(data.getDistance())
+            else:
+                print("Not Define {} in defineFigName".format(classifyValue.name))
+                sys.exit(1)
+
+        return figName
+
+    def defineXlabel(self, xValue):
+        if xValue is XValue.DISTANCE:
+            return "Tx-Rx distance(um)"
+        else:
+            print("Not define {} in defineXlabel".format(xValue.name))
+            sys.exit(1)
+
+    def defineYlabel(self, yValue):
+        if yValue is YValue.MEAN:
+            return "Mean RTT (s)"
+        else:
+            print("Not define {} in defineYlabel".format(yValue.name))
+            sys.exit(1)
+
+
+    def drawLineGraph(self, X, Y, labels, xValue, yValue, isMath, figName):
+        for i in range(len(Y)):
+            plt.plot(X, Y[i], color=COLOR_LIST[i], label=labels[i], markersize="5", linestyle=STYLE_LIST[i])
+
+        if xValue is XValue.DISTANCE:
+            plt.xticks(X)
+            plt.legend(loc='upper left')
+        else:
+            print("Not define {} in drawLineGraph".format(xValue.name))
+            sys.exit(1)
+
+        plt.xlabel(self.defineXlabel(xValue))
+        plt.ylabel(self.defineYlabel(yValue))
+        plt.grid(True)
+
+        if isMath:
+            plt.gca().yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
+            plt.gca().ticklabel_format(style="sci", axis="y", scilimits=(0, 0))
+
+        plt.savefig(figName)
+        plt.close('all')
+
     def drawSpecificGraph(self, xValue, yValue, labelValue, classifyValue):
+        dirName = self.defineDirectoryPath(xValue, yValue, classifyValue)
+        print("Make figure in {}".format(dirName))
         # グラフことにファイル名を分ける
         classifyFileList = self.classifyDataFile(self.allFileList, classifyValue)
 
         for fileList in classifyFileList:
+            figName = dirName + "/" + self.defineFigName(classifyValue, self.dataDict[fileList[0]])
+            print("Make figure {}".format(figName))
             X, Y, labels = self.getLabelValues(xValue, yValue, labelValue, fileList)
+            isMath = False
+            if max(max(Y)) > 10 ** 5:
+                isMath = True
 
+            self.drawLineGraph(X, Y, labels, xValue, yValue, isMath, figName)
+        print("Finish making figure in {}".format(dirName))
 
 
 
